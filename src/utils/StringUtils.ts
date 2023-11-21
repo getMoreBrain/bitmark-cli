c2bc040f27290e74ebc1b8ecfa9e8870da1f8431
Richard Sewell <10079653+zx-ncoderz@users.noreply.github.com>
2023-11-21 13:36:48 +1300

    chore: Updated to v1.4.15 of the parser-generator

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,5 +1,11 @@
 class StringUtils {
-  isString(obj: unknown): boolean {
+  /**
+   * Determines whether the given object is a string.
+   *
+   * @param obj - The object to check.
+   * @returns `true` if the object is a string, `false` otherwise.
+   */
+  public isString(obj: unknown): boolean {
     return typeof obj === 'string' || obj instanceof String;
   }
 }
