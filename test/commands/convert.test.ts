a8e07067e8dfd8c22333b1ee8af9294bdf4e566f
six5536 <10079653+six5536@users.noreply.github.com>
2024-05-06 17:11:05 +1200

    chore: bumped to version 1.7.0 - fixed test for this new version

--- test/commands/convert.test.ts
+++ test/commands/convert.test.ts
@@ -7,10 +7,10 @@ describe('convert', () => {
       'convert',
       '-v',
       '2',
-      '[{"bitmark": "[.article] Hello World","bit": { "type": "article", "format": "bitmark--", "body": "Hello World" }}]',
+      '[{"bitmark": "[.article] Hello World","bit": { "type": "article", "format": "bitmark--", "bitLevel": 1, "body": "Hello World" }}]',
     ])
     .it(
-      'bitmark convert \'[{"bitmark": "[.article] Hello World","bit": { "type": "article", "format": "bitmark--", "body": "Hello World" }}]\'',
+      'bitmark convert \'[{"bitmark": "[.article] Hello World","bit": { "type": "article", "format": "bitmark--", "bitLevel": 1, "body": "Hello World" }}]\'',
       (ctx) => {
         expect(ctx.stdout.replace(new RegExp('[\\n]', 'g'), '')).to.equal('[.article]Hello World');
       },
@@ -22,7 +22,7 @@ describe('convert', () => {
     .command(['convert', '-v', '2', '[.article] Hello World'])
     .it("bitmark convert '[.article] Hello World'", (ctx) => {
       expect(ctx.stdout).to.contain(
-        '"bit":{"type":"article","format":"bitmark--","body":"Hello World"}',
+        '"bit":{"type":"article","format":"bitmark--","bitLevel":1,"body":"Hello World"}',
         // '[{"bit":{"type":"article","format":"bitmark--","body":"Hello World"},"bitmark":"[.article] Hello World"}]\n',
       );
     });
